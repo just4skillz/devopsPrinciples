11362e7e64163aa48fb1e15b64b2325c6b30eeae
Octavian Hera <remus.hera@gmail.com>
2018-06-15 23:10:39 +0300

    Initial commit

---
+++ src/main/java/com/dissertation/devops/App.java
@@ -0,0 +1,13 @@
+package com.dissertation.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
