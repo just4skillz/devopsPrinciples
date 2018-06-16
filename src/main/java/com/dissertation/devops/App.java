bc84ba851d95d2a1e34d54c6f71a74bb8055bc10
Octavian Hera <remus.hera@gmail.com>
2018-06-16 13:38:11 +0300

    Change hello world message in order to test jenkins integration

--- src/main/java/com/dissertation/devops/App.java
+++ src/main/java/com/dissertation/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from main class!" );
+        System.out.println( "Hello from main class!" );
     }
 }
